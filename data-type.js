2dac4bc639858dbfedd11db74fa884ef74fbb8bd
naqeebkns <shajiekns@yahoo.com>
2024-05-03 01:44:57 +0500

    4th class files add at github

---
+++ data-type.js
@@ -0,0 +1,5 @@
+var myAge = 50;
+var myName = "K.Naqeeb";
+var isjobless = false;
+console.log(myAge, myName, isjobless);
+console.log(myName.toUpperCase());
---
+++ data-type.ts
@@ -0,0 +1,6 @@
+let myAge:number=50;
+let myName:string="K.Naqeeb";
+let isjobless:boolean=false;
+console.log(myAge,myName,isjobless);
+console.log(myName.toUpperCase());
+
